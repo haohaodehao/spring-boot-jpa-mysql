667baed2b025a18eb199e511e73906133487197c
company pc <you@example.com>
2018-12-11 17:53:14 +0800

    add some files

---
+++ src/main/java/java/spring_boot_jpa_mysql/SpringBootJpaMysqlApplication.java
@@ -0,0 +1,12 @@
+package java.spring_boot_jpa_mysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJpaMysqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootJpaMysqlApplication.class, args);
+    }
+}
---
+++ src/test/java/java/spring_boot_jpa_mysql/SpringBootJpaMysqlApplicationTests.java
@@ -0,0 +1,16 @@
+package java.spring_boot_jpa_mysql;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootJpaMysqlApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
